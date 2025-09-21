025b54a4901ddab483ca54e7ecf3106406fd410b
Yair Tal <yair.tal2@gmail.com>
2025-09-21 12:14:55 +0100

    updates made

--- src/processing.py
+++ src/processing.py
@@ -49,8 +49,14 @@ pd.set_option("display.max_columns", None)
 
 #changing type of columns from string to int
 data['Population'] = data['Population'].astype(np.int64)
-data['Average Monthly Salary'] = data['Average Monthly Salary'].astype(np.int64)
-data['Average Cost of Living'] = data['Average Cost of Living'].astype(np.int64)
+data['Average Monthly Salary'] = data['Average Monthly Salary'].astype(np.float64)
+data['Average Cost of Living'] = data['Average Cost of Living'].astype(np.float64)
+data['Avgerage Rent Price'] = data['Avgerage Rent Price'].astype(np.float64)
+data['GDP per Capita'] = data['GDP per Capita'].astype(np.float64)
+data['Unemployment Rate'] = data['Unemployment Rate'].astype(np.float64)
+data['Youth Dependency Ratio'] = data['Youth Dependency Ratio'].astype(np.float64)
+data['Working Age Population '] = data['Working Age Population '].astype(np.float64)
+data['Population Density'] = data['Population Density'].astype(np.float64)
 
 #displaying the total population of each country in the data set
 population_ser = data.groupby('Country')['Population'].sum().sort_values(ascending=False)
@@ -81,8 +87,43 @@ print("\nDifference between cost of living and salary:\n")
 print(data[['City', "avg salary - avg cost of living"]].sort_values(by='avg salary - avg cost of living', ascending=False).head(5))
 
 #plot for showing correlation between Salary and Cost of Living
-fig, ax = plt.subplots()
-ax.scatter(data["Average Monthly Salary"], data["Average Cost of Living"])
-ax.set_xlabel("Salary")
-ax.set_ylabel("Cost of living")
+# fig, (ax, ax2) = plt.subplots(1, 2, figsize=(10, 4))
+# ax.scatter(data["Average Monthly Salary"], data["Average Cost of Living"])
+# ax.set_xlabel("Salary")
+# ax.set_ylabel("Cost of living")
+# ax.set_title("Salary versus Cost of Living")
+# plt.show()
+
+plt.scatter(x=data["Average Monthly Salary"], y=data["Average Cost of Living"])
+plt.xlabel('Salary')
+plt.ylabel('Cost of Living')
 plt.show()
+
+languages = {}
+for item in data['Main Spoken Languages']:
+    lst = item.split(",")
+    for i in lst:
+        i = i.strip()
+        if i not in languages:
+            languages[i] = 1
+        else:
+            languages[i] += 1
+
+languages = sorted(languages.items(), key=lambda x: x[1], reverse=True)
+
+spoken_languages, counts = map(list, zip(*languages))
+
+# ax2.bar(x=spoken_languages, height=counts, width=0.5)
+# ax2.set_title("Spoken languages")
+# plt.xticks(rotation=45)
+# plt.show()
+
+plt.bar(x=spoken_languages, height=counts)
+plt.xticks(rotation=45)
+plt.xlabel('Language')
+plt.ylabel('Amount of Cities')
+plt.show()
+
+# print(data[data['City'] == "Lisbon"][["Average Monthly Salary", "Average Cost of Living"]])
+# print(data[data['City'] == "Zurich"][["Average Monthly Salary", "Average Cost of Living"]])
+# print(data[data['City'] == "Barcelona"][["Average Monthly Salary", "Average Cost of Living"]])
